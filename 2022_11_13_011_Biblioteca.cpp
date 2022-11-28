c8e4a7c1f7760e9ac509cddfe08f9099092b01e3
KikeRuiz01 <zap790@amerike.edu.mx>
2022-11-27 22:03:42 -0600

    Initial commit

---
+++ 2022_11_13_011_Biblioteca.cpp
@@ -0,0 +1,197 @@
+// 2022_11_13_011_Biblioteca.cpp
+// Carlos Enrique Ruiz Peña
+//
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    std::string libro1[3];
+    std::string libro2 [3];
+    std::string libro3 [3];
+    std::string comic1[3];
+    std::string comic2[3];
+    std::string comic3[3];
+    std::string pelicula1[3];
+    std::string pelicula2[3];
+    std::string pelicula3[3];
+    std::string videojuego1[3];
+    std::string videojuego2[3];
+    std::string videojuego3[3];
+
+    std::cout << "Programa de Biblioteca para almacenar tu entretenimiento\n";
+    //libro 1
+    Sleep(777);
+    std::cout << "Libro 1 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> libro1[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> libro1[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> libro1[2];
+    system("cls");
+
+    //libro 2
+    Sleep(777);
+    std::cout << "Libro 2 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> libro2[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> libro2[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> libro2[2];
+    system("cls");
+
+    //libro 3
+    Sleep(777);
+    std::cout << "Libro 3 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> libro3[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> libro3[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> libro3[2];
+    system("cls");
+
+    //Comic 1
+    Sleep(777);
+    std::cout << "Comic 1 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> comic1[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> comic1[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> comic1[2];
+    system("cls");
+
+    //Comic 2
+    Sleep(777);
+    std::cout << "Comic 2 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> comic2[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> comic2[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> comic2[2];
+    system("cls");
+
+    //Comic 3
+    Sleep(777);
+    std::cout << "Comic 3 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> comic3[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> comic3[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> comic3[2];
+    system("cls");
+
+    //Pelicula 1
+    Sleep(777);
+    std::cout << "Pelicula 1 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> pelicula1[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> pelicula1[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> pelicula1[2];
+    system("cls");
+
+    //Pelicula 2
+    Sleep(777);
+    std::cout << "Pelicula 2 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> pelicula2[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> pelicula2[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> pelicula2[2];
+    system("cls");
+
+    //Pelicula 3
+    Sleep(777);
+    std::cout << "Pelicula 3 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> pelicula3[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> pelicula3[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> pelicula3[2];
+    system("cls");
+
+    //Videojuego 1
+    Sleep(777);
+    std::cout << "Videojuego 1 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> videojuego1[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> videojuego1[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> videojuego1[2];
+    system("cls");
+
+    //Videojuego 2
+    Sleep(777);
+    std::cout << "Videojuego 2 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> videojuego2[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> videojuego2[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> videojuego2[2];
+    system("cls");
+
+    //Videojuego 3
+    Sleep(777);
+    std::cout << "Videojuego 3 \n";
+    Sleep(777);
+    std::cout << "Ingrese el titulo \n";
+    std::cin >> videojuego3[0];
+    std::cout << "Ingrese el año de publicacion \n";
+    std::cin >> videojuego3[1];
+    std::cout << "Ingrese el autor \n";
+    std::cin >> videojuego3[2];
+    system("cls");
+
+    //Libros
+    std::cout << "Libros" << std::endl;
+    std::cout << "Titulo: " << libro1[0] << ". Año: " << libro1[1] << ". Autor: " << libro1[2] << std::endl;
+    std::cout << "Titulo: " << libro2[0] << ". Año: " << libro2[1] << ". Autor: " << libro2[2] << std::endl;
+    std::cout << "Titulo: " << libro3[0] << ". Año: " << libro3[1] << ". Autor: " << libro3[2] << std::endl;
+
+    //Comics
+    std::cout << "Comics" << std::endl;
+    std::cout << "Titulo: " << comic1[0] << ". Año: " << comic1[1] << ". Autor: " << comic1[2] << std::endl;
+    std::cout << "Titulo: " << comic2[0] << ". Año: " << comic2[1] << ". Autor: " << comic2[2] << std::endl;
+    std::cout << "Titulo: " << comic3[0] << ". Año: " << comic3[1] << ". Autor: " << comic3[2] << std::endl;
+
+    //Peliculas
+    std::cout << "Peliculas" << std::endl;
+    std::cout << "Titulo: " << pelicula1[0] << ". Año: " << pelicula1[1] << ". Autor: " << pelicula1[2] << std::endl;
+    std::cout << "Titulo: " << pelicula2[0] << ". Año: " << pelicula2[1] << ". Autor: " << pelicula2[2] << std::endl;
+    std::cout << "Titulo: " << pelicula3[0] << ". Año: " << pelicula3[1] << ". Autor: " << pelicula3[2] << std::endl;
+
+    //Videojuegos
+    std::cout << "Videojuegos" << std::endl;
+    std::cout << "Titulo: " << videojuego1[0] << ". Año: " << videojuego1[1] << ". Autor: " << videojuego1[2] << std::endl;
+    std::cout << "Titulo: " << videojuego2[0] << ". Año: " << videojuego2[1] << ". Autor: " << videojuego2[2] << std::endl;
+    std::cout << "Titulo: " << videojuego3[0] << ". Año: " << videojuego3[1] << ". Autor: " << videojuego3[2] << std::endl;
+    
+
+
+}
+
+
